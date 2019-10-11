c8e61338a2e7ca5d83fa904dad0d2b6923b1377b
Michel Junior <jrcassunde95@gmail.com>
2019-10-11 14:59:18 -0300

    Implemetando o Moldel e o bean Validation

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,13 +1,28 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
 
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) { //BindingResult : Verifica se há erros. @Valid faz as validações implementadas no model.
+		if(result.hasErrors()) {
+			System.out.println(">>>> tem erro sim!");
+		}
+		System.out.println(">>> sku: " + cerveja.getSku());
 		return "cerveja/CadastroCerveja";
 	}
 	
